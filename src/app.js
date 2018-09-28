921b431bfd25ba84750ff55047de11f6436aa9c7
Ratish <ratish.jayemb@gmail.com>
2018-09-28 17:51:06 +0530

    Completed Module

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const express = require('express');
 
 const app = express();
 
-app.set('views',path.join('__dirname','/src/views'));
+app.set('views', path.join(__dirname,'/src/views'));
 app.set('view engine','ejs');
 
 app.use(express.static(path.join(__dirname, '/public')));
